10981db20708e7610d8fcf6974e3b4332c6b1277
kaplanyaniv <yaniv.224@gmail.com>
2022-09-22 17:35:26 +0300

    better if condition

--- cfy_lint/yamllint_ext/utils.py
+++ cfy_lint/yamllint_ext/utils.py
@@ -298,7 +298,7 @@ def import_cloudify_yaml(import_item, base_path=None):
     for k in result.keys():
         left = 'imported_{}'.format(k)
         if left not in context:
-            if isinstance(result[k], dict):
+            if isinstance(result[k], dict) and left in ['imported_node_types']:
                 context[left] = list(result[k].keys())
             else:
                 context[left] = result[k]
