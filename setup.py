b5f5b5a832fd2db8a539432934d84dfee3eb9661
kaplanyaniv <yaniv.224@gmail.com>
2023-01-11 16:15:19 +0200

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import (setup, find_packages)
 
 setup(
     name='cfy-lint',
-    version='0.0.22',
+    version='0.0.23',
     license='LICENSE',
     packages=find_packages(),
     description='Linter for Cloudify Blueprints',
