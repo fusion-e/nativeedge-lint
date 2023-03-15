9e501f9fe41ef2b81984ea208e4b867268042a28
Nelynehemia <nelyne1014@gmail.com>
2023-03-15 12:04:51 +0200

    Revert "add version"
    This reverts commit 0df78432175e3898289dc3bd44942bb68319131b.

--- cfy_lint/helptexts.py
+++ cfy_lint/helptexts.py
@@ -31,5 +31,3 @@ Not all issues can be solved automatically."""
 v = """Show verbose output, including exceptions."""
 
 xs = """Do not display suggested values for supported sections."""
-
-version = """cfy-lint version. """
