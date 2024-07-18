e9378f2bb482ac6de86f53da2981ea9e2242603a
kaplanyaniv <yaniv.224@gmail.com>
2024-07-18 15:25:30 +0300

    flake8

--- ne_lint/yamllint_ext/autofix/utils.py
+++ ne_lint/yamllint_ext/autofix/utils.py
@@ -90,8 +90,10 @@ def build_diff_lines():
 
 def connect_two_sorted_dicts(dict1, dict2):
     result_dict = {}
+    # flake8: noqa
     keys1 = iter(dict1) #gitleaks:allow
     keys2 = iter(dict2) #gitleaks:allow
+    
     key1 = next(keys1, None)
     key2 = next(keys2, None)
 
